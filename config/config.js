98c6bb70ed25debeac9d22407ba2825c7254f004
Angel Deluna <angelluna2016@gmail.com>
2021-04-09 15:14:57 -0400

    add react project

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+module.exports =
+{
+  "development": {
+    "username": "postgres",
+    
+    "password": process.env.PASSWORD || null,
+    "database": "full-stack",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "use_env_variable": "DATABASE_URL"
+  }
+}
