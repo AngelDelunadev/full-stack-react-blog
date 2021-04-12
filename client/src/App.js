11a19e170b691594f71a65be8f3253383a8e46ce
Angel Deluna <angelluna2016@gmail.com>
2021-04-12 01:06:42 -0400

    added post table

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import Menu from '@material-ui/icons/Menu';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" style={{ marginRight: 'auto' }}>
+              Full Stack Blog
+            </Typography>
+            <Button color="inherit" component={NavLink} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={NavLink} to="/register">
+              Register
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Container style = {{margin: "2em auto"}}>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Container>
+
+      </Router>
     </div>
   );
 }
-
 export default App;
